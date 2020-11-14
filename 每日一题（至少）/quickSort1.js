9b0ae3558d9b5986b4cce157cd1518ef9da56c4b
cielliu <ljnbupt@gmail.com>
2020-11-14 09:30:26 +0800

    2020/11/13

---
+++ 每日一题（至少）/quickSort1.js
@@ -0,0 +1,53 @@
+function partition(array, left, right){
+
+    let pivot = array[left];
+    let i = left;
+    let j = right;
+
+    while(i < j){
+        while(i < j && array[j] > pivot){
+            j--;
+        }
+
+        arr[i] = arr[j];
+
+        while(i < j && array[i] < pivot){
+            i++;
+        }
+
+        arr[j] = arr[i];
+        
+    }
+    array[i] = pivot; 
+    return i;
+}
+var count = 0;
+
+function quick(array, left, right){
+    if(left < right){
+        let flag = partition(array, left, right);
+        quick(array, left, flag - 1);
+        quick(array, flag + 1, right);
+        
+    }
+
+    return array;
+      
+}
+
+function quickSort(array){
+    quick(array, 0, array.length - 1);
+
+}
+
+function swap(array, x, y){
+    let temp = array[x];
+    array[x] = array[y];
+    array[y] = temp;
+
+}
+
+
+let arr = [2, 6, 1,7,8];
+quickSort(arr);
+console.log(arr);
