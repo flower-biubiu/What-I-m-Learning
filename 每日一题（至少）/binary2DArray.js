24114ba291f8578a24edb381dde8c087522ab43a
cielliu <ljnbupt@gmail.com>
2020-11-16 16:53:20 +0800

    2020/11/16

---
+++ 每日一题（至少）/binary2DArray.js
@@ -0,0 +1,75 @@
+方法一：
+
+function search (array, target){
+    if(array.length < 1 || array[0].length < 1){
+        return false;
+    }
+
+    let row = 0;
+    let col = array[0].length;
+
+    while(row <= array.length - 1 && col >= 0 ){
+        if(array[row][col] === target){
+            return true;
+        }else if(array[row][col] < target){
+            row++;
+        }
+        else{
+            col--;
+        }
+
+    }
+    return false;
+
+
+
+
+}
+
+let arr = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+console.log (search(arr, 10));
+
+方法二：
+
+function search(array, target){
+    let m = array.length;
+    let n = array[0].length;
+
+    for(let i = 0; i < array.length; i++){
+        let temp = array[i];
+        let temp_length = temp.length;
+
+        if(temp[temp_length - 1] > target){
+
+            if(find(temp, 0, temp_length - 1, target)){
+                return true;
+            }
+
+        }
+    }
+
+    return false;
+
+}
+
+function find(array, low, high, target){
+    if(low > high){
+        return false;
+    }
+
+    let mid = Math.floor((low + high) / 2);
+
+    if(array[mid] === target){
+        return true;
+    }else if(array[mid] < target){
+        return find(array, mid + 1, high, target);
+    }else{
+        return find(array, low, mid - 1, target);
+    }
+
+}
+
+let arr = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+console.log (search(arr, 4));
--- 每日一题（至少）/binarySearch.js
+++ 每日一题（至少）/binarySearch.js
@@ -1,15 +1,26 @@
 function find(array, left, right, target){
-    let index = Math.floor( array.length / 2 );
+    let index = Math.floor( (left + right)/2 );
     let flag = array[index];
 
     if(flag === target){
         return index;
     }else if(flag > target){
-        return find(array, left, flag - 1);
+        return find(array, left, index - 1, target);
     }else{
-        return find(array, flag + 1, right);
+        return find(array, index + 1, right, target);
     }
 }
+// function bsearch(array,low,high,target)
+// {
+//     if (low > high) return -1;
+//     var mid = Math.floor((low + high)/2);
+//     if (array[mid]> target){
+//         return  bsearch(array, low, mid -1, target);
+//     } else if (array[mid]< target){
+//         return  bsearch(array, mid+1, high, target);
+//     }else{return mid;}
+        
+// }
 
 let arr = [1,3,12, 14, 15, 23, 24, 26, 30];
-console.log(find(arr, 0, arr.length - 1, 15));
+console.log(find(arr, 0, arr.length - 1, 3));
